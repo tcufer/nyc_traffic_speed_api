a5ae82d8695b3100692e768dc878b7540aa126c7
zamot101 <zamot101@gmail.com>
2016-01-26 17:27:45 +0100

    Sensor data available for specific date.

--- nyc_traffic_speed_api/__init__.py
+++ nyc_traffic_speed_api/__init__.py
@@ -1,10 +1,30 @@
 from flask import Flask, make_response, jsonify
 from flask.ext.restful import Api
+from flask.ext.pymongo import PyMongo
+from errors import ValidationError, not_found
 import pymongo
 import types
 
 app = Flask(__name__)
 api = Api(app)
+# mongo = PyMongo(app)
+
+
+
+# connect to another MongoDB server altogether
+app.config['MONGO_HOST'] = '127.0.0.1'
+app.config['MONGO_PORT'] = 27017
+app.config['MONGO_DBNAME'] = 'dev_nyc_traffic_speed'
+mongo = PyMongo(app, config_prefix='MONGO')
+
+@app.errorhandler(404)
+def not_found_error(e):
+    return not_found('Not found')
+
+
+# @app.errorhandler(400)
+# def bad_request_error(e):
+#     return bad_request('invalid request')
 
 def mongodb_conn():
 		try:
--- nyc_traffic_speed_api/resources/speed_data.py
+++ nyc_traffic_speed_api/resources/speed_data.py
@@ -1,10 +1,10 @@
-import requests, csv, pymongo, sys, json, logging
+import requests, csv, pymongo, sys, json, logging, re, pytz
 import datetime
 from flask import jsonify, abort
 from bson import json_util
 from common import send_email
 from flask.ext.restful import Resource
-from nyc_traffic_speed_api import api, db_conn
+from nyc_traffic_speed_api import api, db_conn, not_found_error
 from . import Eastern
 
 logging.basicConfig(level=logging.INFO)
@@ -15,15 +15,14 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 # logger.setLevel(logging.WARNING)
 
-@api.route('/trafficSpeed/<int:id>')	
+@api.route('/trafficSpeed/<int:id>')
 class TrafficSpeedResource(Resource):
 
 	def get(self, id):
 
-		# get a handle to the nyc_traffic_speed database
-		# db=connection.nyc_traffic_speed
 		sensors = db_conn.sensors
 		links = db_conn.links
+		
 		try:
 			doc = sensors.find({"sensorId": str(id)}, { 	"_id": False,
 															'sensorId': True,
@@ -31,13 +30,11 @@ class TrafficSpeedResource(Resource):
 														   	"speed": True,
 														   	"travelTime": True,
 														   	"linkId": True
-														   	# "borough": True
 														   	}).sort("dataAsOf", -1).limit(1)
 		
 		except pymongo.errors, e:
 			return "Unexpected error: ", e	
-			# return status code
-		# speed_sensor = [sensor for sensor in doc]
+
 		EST = Eastern()
 		speed_sensor = []
 		for sensor in doc:
@@ -54,13 +51,81 @@ class TrafficSpeedResource(Resource):
 		return jsonify({'speedSensor': speed_sensor})
 
 
+@api.route('/trafficSpeed/<int:id>/<string:date>')
+class TrafficSpeedByDateResource(Resource):
+
+	def get(self, id, date):
+
+		if date is None:
+			return jsonify({'test': "date parameter missing"})
+		if not re.search("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$",  date):
+			abort(404)
+
+		else:
+
+			sensors = db_conn.sensors
+			links = db_conn.links
+			localtz = pytz.timezone('America/New_York')
+			date = localtz.localize(datetime.datetime.strptime(date, "%Y-%m-%d"))
+			end_date = date + datetime.timedelta(days=1)
+
+			try:
+
+				doc = sensors.aggregate(
+						[
+							{ 
+								"$match": { "sensorId" : str(id), "dataAsOf": {"$gte": date, "$lt": end_date}  }
+							},
+							{
+								"$group":
+									{
+										"_id": {"sensorId": "$sensorId", "linkId": "$linkId"},
+										"measures": { "$push": {"timestamp": "$dataAsOf", "speed": "$speed", "travelTime": "$travelTime"}}
+
+									}
+							},
+							{
+								"$project":
+									{
+										"_id": 0,
+										"sensorId": "$_id.sensorId",
+										"linkId" : "$_id.linkId",
+										"measures": 1
+									}
+							}
+						]
+					)
+
+
+			except pymongo.errors, e:
+				return "Unexpected error: ", e	
+
+			EST = Eastern()
+			speed_sensor = []
+			for sensor in doc['result']:
+				try:
+					l = links.find({"linkId": sensor["linkId"]}, {"linkName"})	
+					sensor["linkName"] = l[0]["linkName"]
+					measures = sensor['measures']
+					sensor['measures'] = []
+					for m in measures:
+						sensor['measures'].append({ 'timestamp': m['timestamp'].astimezone(EST).strftime('%Y-%m-%d %H:%M:%S %Z'),
+													'speed': m['speed'],
+													'travelTime': m['travelTime'] 
+												 })
+					speed_sensor.append(sensor)
+				except pymongo.errors, e:
+					return "Unexpected error: ", e
+			if len(speed_sensor) == 0:
+				print "jazbec"
+				abort(404)
+			return jsonify({'speedSensor': speed_sensor})
+
 @api.route('/trafficSpeed')	
 class TrafficSpeedListResource(Resource):
 
 	def get(self):
 
-		# get a handle to the nyc_traffic_speed database
-		# db=connection.nyc_traffic_speed
 		sensors = db_conn.sensors
 		links = db_conn.links
 
@@ -78,24 +143,11 @@ class TrafficSpeedListResource(Resource):
 		except pymongo.errors, e:
 			print "Unexpected error: ", e	
 
-		# try:
-		# 	linkNames = links.find({},{"linkId", "linkName"})
-		# except pymongo.errors, e:
-		# 	print "Unexpected error: ", e	
-
-		# link_names = dict()
-		# for name in linkNames:
-		# 	link_names[name["linkId"]] = name["linkName"]
-		# 	# print "1" 
 		EST = Eastern()
 		speed_sensors_list = []
 		for point in documents['result']:
 			point['dataAsOf'] = point['dataAsOf'].astimezone(EST).strftime('%Y-%m-%d %H:%M:%S %Z')
 			speed_sensors_list.append(point)	
-		# speed_sensors_list = [point for point in documents['result']]
-		
-		# for point in documents['result']:
-		# 	speed_sensors_list
 
 		if len(speed_sensors_list) == 0:
 			abort(404)
